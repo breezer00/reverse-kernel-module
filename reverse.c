7fcab179a7f781ff6c69f25ab0296d31d7f78a8a
Yanlei Zhao <breezer00@gmail.com>
2014-06-03 23:19:06 -0700

    Add Makefile

--- reverse.c
+++ reverse.c
@@ -2,27 +2,28 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-MODULE_LECENSE("GPL");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yanlei Zhao <breezer00@gmail.com>");
 MODULE_DESCRIPTION("In-kernel phrase reverser");
 
 /* Accept user defined buffer size */
 static unsigned long buffer_size = 8192;
 module_param(buffer_size, ulong, (S_IRUSR|S_IRGRP|S_IROTH));
-MODULE_PARAM_DESC(buffer_size, "Internal buffer size");
+MODULE_PARM_DESC(buffer_size, "Internal buffer size");
 
 static int __init reverse_init(void)
 {
   if (!buffer_size)
     return -1;
-  printk(KERN_INFO "reverse device has been registered, buffer
-         size if %lu bytes\n", buffer_size);
+  printk(KERN_INFO
+      "reverse device has been registered, buffer size is %lu bytes\n", 
+      buffer_size);
   return 0;
 }
 
 static void __exit reverse_exit(void)
 {
-  printk(KERN_INFO "reverse device has been unregistered\n");
+  printk(KERN_INFO"reverse device has been unregistered\n");
 }
 
 
