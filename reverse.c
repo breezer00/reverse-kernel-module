bdf0e03061141bc9f8e7ba7ad2f3ca0bedef0e1f
Yanlei Zhao <breezer00@gmail.com>
2014-06-03 23:06:56 -0700

    reverse.c

---
+++ reverse.c
@@ -0,0 +1,30 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+MODULE_LECENSE("GPL");
+MODULE_AUTHOR("Yanlei Zhao <breezer00@gmail.com>");
+MODULE_DESCRIPTION("In-kernel phrase reverser");
+
+/* Accept user defined buffer size */
+static unsigned long buffer_size = 8192;
+module_param(buffer_size, ulong, (S_IRUSR|S_IRGRP|S_IROTH));
+MODULE_PARAM_DESC(buffer_size, "Internal buffer size");
+
+static int __init reverse_init(void)
+{
+  if (!buffer_size)
+    return -1;
+  printk(KERN_INFO "reverse device has been registered, buffer
+         size if %lu bytes\n", buffer_size);
+  return 0;
+}
+
+static void __exit reverse_exit(void)
+{
+  printk(KERN_INFO "reverse device has been unregistered\n");
+}
+
+
+module_init(reverse_init);
+module_exit(reverse_exit);
